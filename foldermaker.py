24c3849d0a76deb19f38270ea3d7c4da58b9b421
Ryan-Goosen <130756749+RyanGoosen-CapaCiti@users.noreply.github.com>
2023-05-02 10:55:25 +0200

    update

--- foldermaker.py
+++ foldermaker.py
@@ -1,16 +1,17 @@
-import os
+# import os
 
-parent = ""
+# parent = ""
 
-for i in range(3 , 5): #Creates the week folder inside of the parent folder
-    dict = f"Week {i}"
-    path = os.path.join(parent, dict)
-    os.mkdir(path)
-    print(path + " created")
-    for j in range(1 , 6): #Creates the day folder inside of the week folder
-        dict = f"Day {j}"
-        new_parent = path + "/"
-        new_path = os.path.join(new_parent + "/", dict)
-        os.mkdir(new_path)
-        print(path + " created")
+# for i in range(3 , 5): #Creates the week folder inside of the parent folder
+#     dict = f"Week {i}"
+#     path = os.path.join(parent, dict)
+#     os.mkdir(path)
+#     print(path + " created")
+#     for j in range(1 , 6): #Creates the day folder inside of the week folder
+#         dict = f"Day {j}"
+#         new_parent = path + "/"
+#         new_path = os.path.join(new_parent + "/", dict)
+#         os.mkdir(new_path)
+#         print(path + " created")
 
+print("Terminal")
